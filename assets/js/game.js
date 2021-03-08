ffdecde01cb062e40cedb2f9e23021bde5564e84
Elke Harmanny <ElkeHarmanny@outlook.com>
2021-03-08 15:40:30 +0100

    Add the basic shapes game platform

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+// When the DOM has finished loading, start loading the basics for the game.
+document.addEventListener("DOMContentLoaded", function () {
+    loadShapes();
+});
+
+function loadShapes(){
+    let shapes = $('.game-container')[0];
+    console.log(shapes);
+    for(i = 1;i < 10; i++){
+        shapes.innerHTML += `
+            <div><img src="assets/images/shape${i}.png" /></div>`
+    }
+}
