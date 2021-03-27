91c662b9d36724aca52372159229026f98c4410f
Elke Harmanny <ElkeHarmanny@outlook.com>
2021-03-27 10:07:18 +0100

    Add settings (speed, numOfShapes)

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Button event handlers
     $('#play-button').click(function () {
-        playGame(3, 250);
+        playGame();
     });
 
     loadShapes();
@@ -35,17 +35,19 @@ function setShapesToShow(numOfShapes) {
     return ShapesToShow;
 }
 
-function highlightShapes(numOfShapes, speed, shapesToShow) {
+function highlightShapes(shapesToShow) {
     // Set an interval to show the corresponding shapes
-    // Start by setting up some variables that we'll be needing
+    // Retrieve the current game settings
+    let settings = $('#settings').html().split(',');
+    // Now set up some variables that we'll be needing
     let counter = 0;
     let shapeID = '';
-    let speedString = `speed${String(speed)}`;
+    let speedString = `speed${settings[1]}`;
 
     // Start the interval
     let interval = setInterval(function () {
         // Check if we've processed the required amount of shapes
-        if (counter < numOfShapes) {
+        if (counter < settings[0]) {
             // Remove the class from the previous shape shown
             if (shapeID !== '') {
                 $(shapeID).removeClass(speedString);
@@ -64,26 +66,69 @@ function highlightShapes(numOfShapes, speed, shapesToShow) {
         }
         // Increment the counter so we know when to stop
         counter++;
-    }, speed);
+    }, settings[1]);
 }
 // -------- / Shapes functions ------
 
+// -------- Game state functions ------
+function setGameSettings() {
+    // Retrieve the current game settings
+    let settings = $('#settings').html().split(',');
+    // Convert the array entries to numbers
+    // With thanks to Jonas Answeeuw for this trick
+    // https://stackoverflow.com/questions/4437916/how-to-convert-all-elements-in-an-array-to-integer-in-javascript
+    settings = settings.map(Number);
 
-function playGame(numOfShapes, speed) {
-    // Load up the shapes that will be shown
-    let shapes = setShapesToShow(numOfShapes);
-    // Start highlighting those shapes
-    highlightShapes(numOfShapes, speed, shapes);
+    // Set the new game settings
+    // Number of shapes always just gets incremented by one
+    // Speed is incremented at set shape numbers
+    settings[0] += 1;
+    switch (settings[0]) {
+        case 3:
+            settings[1] = 1000;
+            break;
+        case 5:
+            settings[1] = 750;
+            break;
+        case 7:
+            settings[1] = 500;
+            break;
+        case 9:
+            settings[1] = 250;
+            break;
+        case 11:
+            settings[1] = 150;
+            break;
+        case 13:
+            settings[1] = 100;
+            break;
+        default:
+            settings[1] = 1000;
+            break;
+    }
+
+    // Now write the new values back to the div
+    $('#settings').html(`${settings}`);
 }
 
-function displayResults(){
+function playGame() {
+    // Update the game settings
+    setGameSettings();
+    // Retrieve the game settings
+    let settings = $('#settings').html().split(',');
+    // Load up the shapes that will be shown
+    let shapes = setShapesToShow(settings[0]);
+    // Start highlighting those shapes
+    highlightShapes(shapes);
+}
+
+function displayResults() {
     // Make results pane visible
     let resultsPane = $('#results-pane');
     resultsPane.removeClass('hidden');
 
     // Retrieve the results from the div
-    let results = $('#settings').html();
-    results = results.split(',');
+    let results = $('#settings').html().split(',');
 
     // Display the results  in results pane
     resultsPane.html(`
@@ -92,31 +137,7 @@ function displayResults(){
     You guessed: ${results[0]} shapes right.
     The game speed was set at: ${results[1]} miliseconds.`);
 }
-
-function generateNewRandomNumber(oldNum) {
-    // Create a new random number between 1 and 9
-    // ensure this is a different number from the one inputted by doing
-    // a do while loop until we have a different number. 
-    let newNum;
-    let numCheck = false;
-
-    // Only try to process if the input supplied is a number
-    // Credit to using the Number.isFinite() method for checking goes to:
-    // Marcus Sanatan
-    // https://stackabuse.com/javascript-check-if-variable-is-a-number/
-    if (Number.isFinite(Number(oldNum)) || Number(oldNum) === 0) {
-        do {
-            newNum = Math.floor(Math.random() * 9) + 1
-            if (newNum !== oldNum) {
-                numCheck = true;
-            }
-        } while (numCheck == false);
-
-        return newNum;
-    } else {
-        console.log(`Wrong input supplied. Given: ${Number(oldNum)}`);
-    }
-}
+// -------- / Game state functions ------
 
 // -------- Answer functions ------
 function prepareForAnswers(shapes) {
@@ -137,8 +158,7 @@ function checkAnswer(shape) {
     // Get all the answers from the div
     // Take out the first one to check against 
     // the shape that was clicked
-    let answers = $('#answers').html();
-    answers = answers.split(',');
+    let answers = $('#answers').html().split(',');
     let correctAnswer = Number(answers[0]);
 
     // Get the number from the shape
@@ -164,3 +184,30 @@ function checkAnswer(shape) {
     }
 }
 // -------- / Answer functions ------
+
+// -------- Helper functions ------
+function generateNewRandomNumber(oldNum) {
+    // Create a new random number between 1 and 9
+    // ensure this is a different number from the one inputted by doing
+    // a do while loop until we have a different number. 
+    let newNum;
+    let numCheck = false;
+
+    // Only try to process if the input supplied is a number
+    // Credit to using the Number.isFinite() method for checking goes to:
+    // Marcus Sanatan
+    // https://stackabuse.com/javascript-check-if-variable-is-a-number/
+    if (Number.isFinite(Number(oldNum)) || Number(oldNum) === 0) {
+        do {
+            newNum = Math.floor(Math.random() * 9) + 1
+            if (newNum !== oldNum) {
+                numCheck = true;
+            }
+        } while (numCheck == false);
+
+        return newNum;
+    } else {
+        console.log(`Wrong input supplied. Given: ${Number(oldNum)}`);
+    }
+}
+// -------- / Helper functions ------
